1bbf5c6f3f381570448a984e2b5c78fb1094f2a5
Reagan Zierke <reaganzierke@gmail.com>
2024-09-25 09:41:38 -0500

    e

--- In Class Stuff/tip.py
+++ In Class Stuff/tip.py
@@ -8,7 +8,7 @@ A Program that calculates the total price based on user input of meal cost and t
 #Takes user input for the cost of the meal and the tip percentage
 #Converts tip percentage into a decimal to multiply by
 cost = float(input("What is the cost of the meal? "))
-tip_percent = 0.1*float(input("What percent would you like to tip? "))
+tip_percent = 0.1*float(input("What percentage would you like to tip? "))
 
 new_cost = cost * 1+tip_percent
 print(f"\nThe total cost is{new_cost: .2f}")
